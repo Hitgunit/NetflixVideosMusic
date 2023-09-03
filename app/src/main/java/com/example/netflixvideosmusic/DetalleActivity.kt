d4eb077a3e3d641d386cee19bfbb1898e1535f53
Adair Duarte <ada_dp10@hotmail.com>
2023-09-03 00:58:32 -0600

    AddBoton
    Se agrego el boton correspondiente para que pueda quitar el anuncio

--- app/src/main/java/com/example/netflixvideosmusic/DetalleActivity.kt
+++ app/src/main/java/com/example/netflixvideosmusic/DetalleActivity.kt
@@ -16,74 +16,119 @@ import kotlin.random.Random
 
 class DetalleActivity : AppCompatActivity() {
 
-    lateinit var txtTittle: TextView
-    lateinit var txtDetalle: TextView
-    lateinit var btnBack: Button
-    lateinit var videoView: VideoView
-    lateinit var btnSaltar: Button
+    private lateinit var txtTitle: TextView
+    private lateinit var txtDetail: TextView
+    private lateinit var btnBack: Button
+    private lateinit var videoView: VideoView
+    private lateinit var btnSaltar: Button
+
+    private var anuncioPlaying = false
+
+    private val tiempoSaltar = 10000L
+
+    //Guarda la posicion del video
+    private var videoPosition: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
 
-        txtTittle = findViewById(R.id.txtTittle)
-        txtDetalle = findViewById(R.id.txtDetalle)
+        txtTitle = findViewById(R.id.txtTittle)
+        txtDetail = findViewById(R.id.txtDetalle)
         btnBack = findViewById(R.id.btnBack)
         videoView = findViewById(R.id.videoView)
         btnSaltar = findViewById(R.id.btnSaltar)
-        btnSaltar.visibility = View.INVISIBLE
 
+        //Se oculta el boton
+        btnSaltar.visibility = View.INVISIBLE
 
         btnBack.setOnClickListener {
             onBackPressed()
         }
 
-        //Se inicia el fun para obtener la informacion
         Informacion()
     }
 
-    fun Informacion(){
-        //Se obtiene el obejto con los datos recibidos
-        val data = intent.getParcelableExtra<Media>("media")
-        if (data != null){
-            //Se asigna los valores name y description
-            txtTittle.text = data.name
-            txtDetalle.text = data.description
+    private fun Random(min: Long, max: Long): Long {
+        return Random.nextLong(min, max)
+    }
 
+    private fun Informacion() {
+        val data = intent.getParcelableExtra<Media>("media") ?: return
 
+        txtTitle.text = data.name
+        txtDetail.text = data.description
 
-            //Se añade el URL de uri
-            val uri = Uri.parse("android.resource://$packageName/${data.url}")
+        val uri = Uri.parse("android.resource://$packageName/${data.url}")
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
 
-            //Se asgina el mediacontroller
-            val mediaController = MediaController(this)
-            mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
 
-            //Se le asigna el Uri deseado
-            videoView.setMediaController(mediaController)
-            videoView.setVideoURI(uri)
-            videoView.requestFocus()
-            videoView.start()
-
-            videoView.setOnPreparedListener{
-                    GlobalScope.launch(Dispatchers.Main){
-                        //Tiempo aleatorio en milisegundos
-                        delay(Random.nextLong(5000, videoView.duration.toLong()))
-                        //Se pausa el video
-                        videoView.pause()
-                        //Se agrega el URL del comercial
-                        val adUri = Uri.parse("android.resource://$packageName/${R.raw.comercial}")
-                        videoView.setVideoURI(adUri)
-                        videoView.start()
-                        //Se espera los 10 segundos para mostrar el boton
-                        delay(10000)
-
-
+        videoView.setOnPreparedListener {
+            if (!anuncioPlaying) {
+                anuncioPlaying = true
+                GlobalScope.launch(Dispatchers.Main) {
+                    val tiempo = Random(2000L, 10000L)
+                    delay(tiempo)
+                    //Guarda la posicion del video
+                    videoPosition = videoView.currentPosition
+                    videoView.pause()
+                    //Agrega el video de anuncio
+                    val adUri = Uri.parse("android.resource://$packageName/${R.raw.comercial}")
+                    videoView.setVideoURI(adUri)
+                    videoView.start()
+                    //Tarda los 10s para iniciar el boton
+                    delay(tiempoSaltar)
+                    //Manda llama rle boton
+                    SaltarAnuncio()
                 }
             }
-
-
+        }
+        //Vuelve al video original si no se presiona el boton
+        videoView.setOnCompletionListener {
+            if (anuncioPlaying) {
+                val originalUri = Uri.parse("android.resource://$packageName/${data.url}")
+                videoView.setVideoURI(originalUri)
+                //Reaunuda el video en el segundo que se pauso
+                videoView.setOnPreparedListener { mp ->
+                    mp.seekTo(videoPosition)
+                    videoView.start()
+                }
+                btnSaltar.visibility = View.INVISIBLE
+                anuncioPlaying = false
+            }
         }
     }
 
+    private fun SaltarAnuncio() {
+        //Se muestra y se deshabilita el boton
+        btnSaltar.visibility = View.VISIBLE
+        btnSaltar.isEnabled = false
 
+        //Inicia otra corrutina
+        GlobalScope.launch(Dispatchers.Main) {
+            //Cuenta regresiva para saltar el anuncio
+            for (i in 5 downTo 0) {
+                delay(1000)
+                btnSaltar.text = "Saltar en $i s"
+            }
+            btnSaltar.text = "Saltar Ahora"
+            btnSaltar.isEnabled = true
+
+            btnSaltar.setOnClickListener {
+                //Se obtine ela informaicon del intent para volver al video original
+                val data = intent.getParcelableExtra<Media>("media") ?: return@setOnClickListener
+                val originalUri = Uri.parse("android.resource://$packageName/${data.url}")
+                videoView.setVideoURI(originalUri)
+                // Reanuda el vídeo en la posición guardada
+                videoView.seekTo(videoPosition)
+                videoView.start()
+                btnSaltar.visibility = View.INVISIBLE
+                anuncioPlaying = false
+            }
+        }
+    }
 }
